df9c22541b2258088b0a9acb72f6e2f9410cd8e4
jpribyl <pribylsnbits@gmail.com>
2018-04-30 19:27:13 -0600

    added eb database config and req.txt

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -86,16 +86,28 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'beatlstw',
-        'USER': 'j',
-        'PASSWORD': 'ranger',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'beatlstw',
+            'USER': 'j',
+            'PASSWORD': 'ranger',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # REST_FRAMEWORK = {
    # 'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -156,6 +168,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 
 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 5000
