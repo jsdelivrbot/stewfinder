5d24319a33d2a4a88403390c1447ca0966b4f2f3
jpribyl <pribylsnbits@gmail.com>
2018-06-10 18:11:48 -0600

    updated backend prod

--- backend/soops/views.py
+++ backend/soops/views.py
@@ -1,12 +1,16 @@
+import json
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
-from soops.serializers import SoopSerializer
 from soops.models import Soop
+from soops.serializers import SoopSerializer
 
 # Create your views here.
+
+
 class SoopList(generics.ListCreateAPIView):
     """Create a soop item"""
     queryset = Soop.objects.all()
@@ -33,4 +37,91 @@ class SoopDetail(generics.RetrieveUpdateDestroyAPIView):
         :returns: TODO
 
         """
+
         return Soop.objects.all().filter()
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    return ip
+
+
+def GetNumVotes(request, soop_id):
+    """return the votes for a given soup id
+
+    :soop_id: id of the object
+    :returns: integer number of total votes
+
+    """
+    obj = Soop.objects.get(pk=soop_id)
+
+    return HttpResponse(obj.votes.count())
+
+
+def VoteUp(request, soop_id):
+    """return the votes for a given soup id
+
+    :soop_id: id of the object
+    :returns: integer number of total votes
+
+    """
+    obj = Soop.objects.get(pk=soop_id)
+    user_id = request.user.id
+
+    ip = get_client_ip(request)
+    ip_int = int(ip.replace('.', ''))
+
+    return HttpResponse(obj.votes.up(ip_int))
+
+
+def VoteDown(request, soop_id):
+    """return the votes for a given soup id
+
+    :soop_id: id of the object
+    :returns: integer number of total votes
+
+    """
+    obj = Soop.objects.get(pk=soop_id)
+    user_id = request.user.id
+
+    ip = get_client_ip(request)
+    ip_int = int(ip.replace('.', ''))
+
+    return HttpResponse(obj.votes.down(ip_int))
+
+
+def UserVotes(request, soop_id):
+    """return the votes for a given soup id
+
+    :soop_id: id of the object
+    :returns: integer number of total votes
+
+    """
+    obj = Soop.objects.get(pk=soop_id)
+
+    ip = get_client_ip(request)
+    ip_int = int(ip.replace('.', ''))
+
+    results = list(obj.votes.all(ip_int).values())
+    return JsonResponse(results, safe=False)
+
+
+def VoteDelete(request, soop_id):
+    """return the votes for a given soup id
+
+    :soop_id: id of the object
+    :returns: integer number of total votes
+
+    """
+    obj = Soop.objects.get(pk=soop_id)
+
+    ip = get_client_ip(request)
+    ip_int = int(ip.replace('.', ''))
+
+    return HttpResponse(obj.votes.delete(ip_int))
